b7e02676dc5260fb1e92da513ef9e5334fab5034
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-13 13:33:44 -0600

    filtered out places starting with The

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,13 +1,24 @@
 function main() {
-  // Put your code here
+  // created an array of all place names
   const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+  //prints "all place names"
   console.log("All Place Names")
 
-  for (name in names) {
-    console.log(`${names}`)
-  };
-}
+  console.log("")
 
-main();
+  for (let name of names) {
+    //Prints all the Places listed in names
+    console.log(`${name}`)
+  };
+
+  console.log("");
+
+  const theNames = names.filter(name => name.startsWith("The"))
+  for (let name of theNames) {
+    console.log(`${name}`)
+  }
+
+}
+main()
