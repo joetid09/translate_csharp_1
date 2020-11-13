a1b51d10c1f9e1a0f946f09dd61024a812812a6d
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-13 16:03:31 -0600

    created both spell books and are showig correct in the console

--- grimoire/js/grimiore/main.js
+++ grimoire/js/grimiore/main.js
@@ -1,6 +1,73 @@
 function main() {
-  // Put your code here
+  console.log("Do you believe in magic?")
+  console.log("------------------------")
+  console.log("")
 
+  allSpells = GetAllSpells()
+  goodBook = MakeGoodSpellBook(allSpells)
+  evilBook = MakeEvilSpellBook(allSpells)
+
+  DisplaySpellBook(goodBook)
+  console.log("")
+  DisplaySpellBook(evilBook)
+
+}
+
+const DisplaySpellBook = (book) => {
+  console.log(book.Title);
+  for (let aSpell of book.Spells) {
+    console.log(aSpell.Name)
+  }
+}
+
+const MakeGoodSpellBook = (allSpells) => {
+  goodBook = {}
+  goodBook.Title = "Good Book";
+  goodBook.Spells = allSpells.filter(spell => spell.IsEvil == false)
+  return goodBook
+}
+
+const MakeEvilSpellBook = (allSpells) => {
+  evilBook = {}
+  evilBook.Title = "Evil Book";
+  evilBook.Spells = allSpells.filter(spell => spell.IsEvil)
+  return evilBook
+}
+
+
+const GetAllSpells = () => {
+  return [
+    {
+      Name: "Turn enemy into a newt",
+      IsEvil: true,
+      EnergyRequired: 5.1
+    },
+    {
+      Name: "Conjure some gold for a local charity",
+      IsEvil: false,
+      EnergyRequired: 2.99
+    },
+    {
+      Name: "Give a deaf person the ability to heal",
+      IsEvil: false,
+      EnergyRequired: 12.2
+    },
+    {
+      Name: "Make yourself emporer of the universe",
+      IsEvil: true,
+      EnergyRequired: 100.0
+    },
+    {
+      Name: "Convince your relatives your political views are correct",
+      IsEvil: false,
+      EnergyRequired: 2921.5
+    },
+  ]
+}
+
+const SpellBook = {
+  Title: null,
+  Spells: null
 }
 
 main();
