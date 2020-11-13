073a92acff3ac8818806f08583b280d0a476135b
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-13 15:04:14 -0600

    completed a dice rolling app, did not do unicode as returned question marks

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,6 +1,22 @@
 function main() {
-  // Put your code here
-
+  console.log(("Let's roll some dice, baby!"));
+  console.log("---------------------------");
+  for (let i = 0; i < 10; i++) {
+    die1 = Roll(1, 10)
+    die2 = Roll(1, 10)
+    sum = die1 + die2
+    let message = `${die1} + ${die2} = ${sum}`
+    if (die1 === die2) {
+      message += " DOUBLES!"
+    }
+    console.log(message)
+  }
 }
+const Roll = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min - 1)) + min;
+}
+
 
 main();
