f625e057e1e514f5205d50b21700af284e44d91f
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-13 13:21:10 -0600

    array of place names created, for..each loop create to iterate and print all places

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,13 @@
 function main() {
   // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+  console.log("All Place Names")
+
+  for (name in names) {
+    console.log(`${names}`)
+  };
 }
 
 main();
